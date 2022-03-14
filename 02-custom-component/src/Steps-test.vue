6ad30bb860798b590fcd826687167070f077eb57
aicherish <aicherish0320@163.com>
2022-03-14 21:52:07 +0800

    feat: 步骤条组件

---
+++ 02-custom-component/src/Steps-test.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <steps :count="count" :active="active"></steps>
+    <button @click="next">下一步</button>
+  </div>
+</template>
+
+<script>
+import Steps from './Steps.vue'
+export default {
+  name: 'StepsTest',
+  components: {
+    Steps
+  },
+  data() {
+    return {
+      count: 5,
+      active: 0
+    }
+  },
+  methods: {
+    next() {
+      this.active++
+    }
+  }
+}
+</script>
+
+<style scoped></style>
